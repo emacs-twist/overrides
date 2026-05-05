3911769df3f50af3876d6efcb7e78de4316f31b2
Akira Komamura <akira.komamura@gmail.com>
2026-05-05 20:34:22 +0900

    Fix the rime package

--- elisp/rime.nix
+++ elisp/rime.nix
@@ -1,6 +1,6 @@
 # Based on
 # https://github.com/nixos/nixpkgs/tree/2cb1420c66c8e634314ce0abf70680208177f5b4/pkgs/applications/editors/emacs/elisp-packages/melpa-packages.nix
-{ pkgs }:
+{ pkgs, ... }:
 old: {
   nativeBuildInputs = [ pkgs.gcc ];
   buildInputs = old.buildInputs ++ [ pkgs.librime ];
