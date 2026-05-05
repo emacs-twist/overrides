6a149d7c0c49f627b8f3ee79be6d175e8833b855
Akira Komamura <akira.komamura@gmail.com>
2026-05-05 16:33:41 +0900

    Add rime

---
+++ elisp/rime.nix
@@ -0,0 +1,12 @@
+# Based on
+# https://github.com/nixos/nixpkgs/tree/2cb1420c66c8e634314ce0abf70680208177f5b4/pkgs/applications/editors/emacs/elisp-packages/melpa-packages.nix
+{ pkgs }:
+old: {
+  nativeBuildInputs = [ pkgs.gcc ];
+  buildInputs = old.buildInputs ++ [ pkgs.librime ];
+  preBuild = ''
+    make lib CC=$CC MODULE_FILE_SUFFIX=${pkgs.stdenv.hostPlatform.extensions.sharedLibrary}
+    rm *.c Makefile
+    chmod 444 *.so
+  '';
+}
